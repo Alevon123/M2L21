15d1ecefae3c7ca76b62eb7439575fa43eb43a6a
Alevon123 <143946789+Alevon123@users.noreply.github.com>
2023-10-10 18:30:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+tips = [
+    "- Использовать многоразовые товары, такие как многоразовые пакеты, стаканчики и столовые приборы.",
+    "- Переходить на цифровые форматы, чтобы избежать использования бумаги и пластиковых носителей информации.",
+    "- Разделять отходы на перерабатываемые и неперерабатываемые материалы и утилизировать их соответствующим образом.",
+    "- Покупать продукты с минимальным количеством упаковки и выбирать упаковки, которые можно переработать или разложить.",
+    "- Участвовать в акциях по очистке окружающей среды и сознательно выбирать места для отдыха, чтобы не загрязнять природу.",
+    "- Использовать энергосберегающие лампы, бытовую технику и электронику, а также максимально использовать естественное освещение."
+]
+
+async def nauka(ctx):
+    tip = random.choice(tips)
+    message = f"{tip}\n"
+    await ctx.send(message)
+
+
+
+    
+
+bot.run("MTE1Mjk2MDkxMzc3MTYwMjAwMQ.GcINRb.kSNEQDpcDu9g5Q-5DgjPO_nqLzfV5lNwlEOqM0")
